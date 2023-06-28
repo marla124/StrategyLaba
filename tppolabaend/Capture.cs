ba38cb3c48682289a80f9e8f8d207898d7636b70
marla124 <95192268+marla124@users.noreply.github.com>
2023-06-28 13:47:01 +0300

    code programm
    class Program, interface, class Capture, class Robot

---
+++ tppolabaend/Capture.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tppolabaend
+{
+    internal class Capture : ICommandRobot
+    {
+        void ICommandRobot.Act()
+        {
+            Console.WriteLine("Происходит захват предмета");
+        }
+    }
+}
---
+++ tppolabaend/Robot.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tppolabaend
+{
+    internal class Robot
+    {
+        private readonly string _model;
+        private ICommandRobot? _commandRobot;
+
+        public Robot(string model)
+        {
+            _model = model;
+        }
+
+        public void SetCommandRobot(ICommandRobot commandRobot)
+        {
+            _commandRobot = commandRobot;
+        }
+        public void Act() 
+        {
+            if (_commandRobot is null)
+            {
+                Console.WriteLine($"{_model} ничего не делает");
+            }
+            else
+            {
+                Console.WriteLine($"{_model} ");
+                _commandRobot.Act();
+            }
+        }
+    }
+}
